f6f02865d7abb9540ba555edc46c2a85a58944fe
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-10 18:27:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
 let list="";
-for(let element of touristSpots.sort()){
+for(let element of touristSpots.sort((a, b) => {
+    const strip = (bandName) => bandName.replace(/^(a |an |the )/i, '');
+    return strip(a) > strip(b) ? 1 : -1;)){
 
 	 list+="<li>"+element+"</li>";
 
