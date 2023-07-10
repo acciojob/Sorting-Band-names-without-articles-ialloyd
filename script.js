2cd46671da363499682cb8f768fe21abaa44b788
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-10 18:19:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //your code here
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+let list="";
+for(let element of touristSpots.sort()){
 
-document.getElementById("band")=touristSpots.sort();
+	 list+="<li>"+element+"</li>";
+
+}
+
+document.getElementById("band").innerHTML =list;
 
